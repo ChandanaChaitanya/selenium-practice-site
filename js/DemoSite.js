3167de8d40d9b0b5589bf7fb6f1742ed81c41c4b
Lakshmi Chandana Kandepu <chandanachaitanya.kollipara@gmail.com>
2017-09-24 13:59:18 +0530

    Adding JS to open new window

--- js/DemoSite.js
+++ js/DemoSite.js
@@ -14,3 +14,7 @@ window.prompt("Which Selenium Tool do you like the most?","e.g. Selenium IDE");
 function newWindow(){
 window.open("https://www.google.co.in/", "_blank");
 }
+//new google page with specified width and height
+function windowToMaximize(){
+window.open("https://www.google.com/", '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+}
